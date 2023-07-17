350068af85d819ba4db545f59be4a19cef94a057
Dalmas <akumuodhiambo@gmail.com>
2023-07-17 15:16:00 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,31 @@
+
+
+//const divide = function (parameter1, parameter2){
+   // return parameter1/parameter2; };
+
+    //cconsole.log(divide(2000, 100));
+
+const divide = (a, b) => {
+    return a/b
+      };
+
+      console.log(divide(2000,100));
+
+
+
+/*const m =()=> {
+    const a = 2000;
+    const b = 100;
+    return a / b;
+  };
+
+  console.log(m());*/
+    
+
+const square = (parameter) => {
+    return parameter * parameter
+  };
+
+const add = (b,c) => {
+    return b+c
+};
